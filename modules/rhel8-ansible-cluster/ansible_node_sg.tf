a4e483a45b2affa3a05b0c7313bc5a422f213132
wyattb79 <wyattb79@gmail.com>
2021-12-27 16:35:59 +0000

    Allow self traffic in sg

--- modules/rhel8-ansible-cluster/ansible_node_sg.tf
+++ modules/rhel8-ansible-cluster/ansible_node_sg.tf
@@ -3,6 +3,13 @@ resource "aws_security_group" "ansible-node-sg" {
 
   vpc_id = "${aws_vpc.ansible-vpc.id}"
 
+  ingress {
+    from_port = 0
+    to_port = 22
+    protocol = "tcp"
+    self = true
+  }
+
   egress {
     from_port = 0
     to_port = 0
