7c163d1c1dbc4f1cbf85ea5b65de62d1ee85fcd0
wyattb79 <wyattb79@gmail.com>
2021-12-27 02:46:50 +0000

    Add missing equal sign

--- modules/rhel8-ansible-cluster/ansible_vpc.tf
+++ modules/rhel8-ansible-cluster/ansible_vpc.tf
@@ -2,8 +2,7 @@ resource "aws_vpc" "ansible-vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
-  id = "ansible-vpc"
-  tags {
+  tags = {
     Name = "ansible-vpc"
   }
 }
