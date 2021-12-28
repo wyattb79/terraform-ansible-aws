379f4c4b345cf28db3da92836cb15d4a86ad7fe3
wyattb79 <wyattb79@gmail.com>
2021-12-28 01:56:28 +0000

    Remove mistake

--- modules/rhel8-ansible-cluster/ansible_master_node.tf
+++ modules/rhel8-ansible-cluster/ansible_master_node.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ansible_master" {
-  ami = var.Ami
-  instance_type = var.Instance_type
-  key_name = var.Key_name
+  ami = var.ami
+  instance_type = var.instance_type
+  key_name = var.key_name
 
   tags = {
     Name = "AnsibleMaster"
