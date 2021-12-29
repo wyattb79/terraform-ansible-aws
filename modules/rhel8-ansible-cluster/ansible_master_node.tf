80e3001e5f45f5bfae6ff575cf1427a65145b16d
wyattb79 <wyattb79@gmail.com>
2021-12-29 11:37:39 +0000

    Fix user data

--- modules/rhel8-ansible-cluster/ansible_master_node.tf
+++ modules/rhel8-ansible-cluster/ansible_master_node.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ansible_master" {
   ami = var.ami
   instance_type = var.instance_type
   key_name = var.key_name
-  user_data = file("ansible-master-setup.sh")
+  user_data = "${file("ansible-master-setup.sh")}"
 
   tags = {
     Name = "AnsibleMaster"
