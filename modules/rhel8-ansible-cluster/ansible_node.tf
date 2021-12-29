4cc4fa64ddc7645e8e916591b96348e4e7dfc6e3
wyattb79 <wyattb79@gmail.com>
2021-12-29 11:21:01 +0000

    Add node

---
+++ modules/rhel8-ansible-cluster/ansible_node.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "ansible_master" {
+  ami = var.ami
+  instance_type = var.instance_type
+  key_name = var.key_name
+  user_data = file("ansible-master-setup.sh")
+
+  tags = {
+    Name = "AnsibleNode"
+  }
+}
