6bc4279b4e13e53078a6852540faec7b273c06ae
wyattb79 <wyattb79@gmail.com>
2021-12-29 11:32:58 +0000

    Update nodes

--- modules/rhel8-ansible-cluster/ansible_node.tf
+++ modules/rhel8-ansible-cluster/ansible_node.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ansible_node" {
   ami = var.ami
   instance_type = var.instance_type
   key_name = var.key_name
-  user_data = file("ansible-master-setup.sh")
+  user_data = file("ansible-node-setup.sh")
 
   tags = {
     Name = "AnsibleNode"
