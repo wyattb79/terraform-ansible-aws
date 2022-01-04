985d530e74c0751b0998b536f08df00a8a634285
Wyatt <wyattb79@gmail.com>
2022-01-04 16:42:35 -0500

    Update ansible_route.tf

--- modules/rhel8-ansible-cluster/ansible_route.tf
+++ modules/rhel8-ansible-cluster/ansible_route.tf
@@ -2,7 +2,7 @@ resource "aws_route_table" "ansible-route-table" {
   vpc_id = aws_vpc.ansible-vpc.id
 
   route {
-    cidr_block = "0.0.0.0/32"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.ansible-gw.id
   }
 }
