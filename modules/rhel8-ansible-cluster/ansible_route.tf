2210c068390893262a0085c5fc7c53ad3e68c26f
wyattb79 <wyattb79@gmail.com>
2022-01-04 20:48:57 +0000

    Add route table

---
+++ modules/rhel8-ansible-cluster/ansible_route.tf
@@ -0,0 +1,8 @@
+resource "aws_route_table" "ansible-route-table" {
+  vpc_id = aws_vpc.ansible-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/32"
+    gateway_id = aws_internet_gateway.ansible-gw.id
+  }
+}
