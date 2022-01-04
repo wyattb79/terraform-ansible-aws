c8a412b92c646d11fe018fc98d35f6417ecc7f67
Wyatt <wyattb79@gmail.com>
2022-01-04 15:13:22 -0500

    Update ansible_subnet.tf

--- modules/rhel8-ansible-cluster/ansible_subnet.tf
+++ modules/rhel8-ansible-cluster/ansible_subnet.tf
@@ -1,6 +1,7 @@
 resource "aws_subnet" "ansible-subnet" {
   cidr_block = "${cidrsubnet(aws_vpc.ansible-vpc.cidr_block, 3, 1)}"
   vpc_id = "${aws_vpc.ansible-vpc.id}"
+  map_public_ip_on_launch = true
   tags = {
     Name = "ansible-subnet"
   }
