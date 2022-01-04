b1e06f58b71e850a8ea72943c143f67ab38a77d1
Wyatt <wyattb79@gmail.com>
2022-01-04 14:44:06 -0500

    Update ansible_node.tf

--- modules/rhel8-ansible-cluster/ansible_node.tf
+++ modules/rhel8-ansible-cluster/ansible_node.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "ansible_node" {
   key_name = var.key_name
   user_data = file("ansible-node-setup.sh")
 
-  security_groups = [ "${aws_security_group.ansible-node-sg.id}" ]
+  vpc_security_group_ids = [ "${aws_security_group.ansible-node-sg.id}" ]
 
   tags = {
     Name = "AnsibleNode"
