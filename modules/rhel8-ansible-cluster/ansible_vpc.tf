ef7bc6ecee4d69447a5ab426373837c7a184ec0a
Wyatt <wyattb79@gmail.com>
2022-01-04 15:53:29 -0500

    Update ansible_vpc.tf

--- modules/rhel8-ansible-cluster/ansible_vpc.tf
+++ modules/rhel8-ansible-cluster/ansible_vpc.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "ansible-vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
-  main_route_table_id = "${aws_route.ansible-route.id}"
+  main_route_table_id = "${aws_route.ansible-route-table.id}"
   tags = {
     Name = "ansible-vpc"
   }
